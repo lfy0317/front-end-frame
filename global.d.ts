eb9a1079c99dc7379d44c85588ddf5c97278cc4e
liufuyuan <18210049914@163.com>
2024-10-18 04:59:10 +0800

    feat: 修改NODE_ENV类型定义

--- global.d.ts
+++ global.d.ts
@@ -1,17 +1,15 @@
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            NODE_ENV: "development" | "production";
-        }
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NODE_ENV: "development" | "production";
     }
 }
 
+declare module "*.css";
 declare module "*.module.less";
 declare module "*.less" {
     const content: { [className: string]: string };
     export default content;
 }
-declare module "*.css";
 
 declare module "*.svg";
 declare module "*.png";
