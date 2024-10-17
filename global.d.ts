55d5087a0e79ced2b20d888652cbfe36e8e32a8c
wb_liufuyuan <wb_liufuyuan@kuaishou.com>
2024-10-17 20:05:35 +0800

    feat: tailwind

--- global.d.ts
+++ global.d.ts
@@ -1,9 +1,9 @@
 declare module "*.module.less";
-
 declare module "*.less" {
     const content: { [className: string]: string };
     export default content;
 }
+declare module "*.css";
 
 declare module "*.svg";
 declare module "*.png";
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import * as React from "react";
-import reactLogo from "@/assets/images/react.svg";
-import * as styles from "./index.module.less";
+import * as React from 'react';
+import reactLogo from '@/assets/images/react.svg';
+import * as styles from './index.module.less';
 
 const App: React.FC = () => {
     console.log(styles);
-    
+
     return (
         <div className={styles.wrapper}>
             <img src={reactLogo} />
             hello 小柒
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
         </div>
     );
 };
