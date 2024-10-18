be99b86fd8163bdb02c0dc372d040a3fd8e08d27
wb_liufuyuan <wb_liufuyuan@kuaishou.com>
2024-10-18 20:31:53 +0800

    feat: eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import * as React from 'react';
+import * as React from "react";
 import { Button } from "@/components/ui/button";
-import reactLogo from '@/assets/images/react.svg';
-import * as styles from './index.module.less';
+import reactLogo from "@/assets/images/react.svg";
+import * as styles from "./index.module.less";
 
 const App: React.FC = () => {
     console.log(styles);
-
     return (
         <div className={styles.wrapper}>
             <img src={reactLogo} />
