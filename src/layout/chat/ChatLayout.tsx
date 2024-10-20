d10d903c927cc1af511db437f7d8a3dd06e99b1d
liufuyuan <18210049914@163.com>
2024-10-21 07:06:13 +0800

    feat: chat

--- src/layout/chat/ChatLayout.tsx
+++ src/layout/chat/ChatLayout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Check, ChevronsUpDown, GalleryVerticalEnd, Search } from "lucide-react";
+import { Plus, CirclePlus, GalleryVerticalEnd, Search } from "lucide-react";
 
 import {
 	Breadcrumb,
@@ -33,10 +33,12 @@ import {
 	SidebarMenuItem,
 	SidebarProvider,
 	SidebarRail,
-	SidebarTrigger
+	SidebarTrigger,
+	SidebarFooter
 } from "@/components/ui/sidebar";
 import { Outlet } from "react-router-dom";
 import "./ChatLayou.css";
+import { Button } from "@/components/ui/button";
 
 const data = {
 	versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
@@ -178,40 +180,27 @@ export default function ChatLayout() {
 				<SidebarHeader>
 					<SidebarMenu>
 						<SidebarMenuItem>
-							<DropdownMenu>
-								<DropdownMenuTrigger asChild>
-									<SidebarMenuButton
-										size="lg"
-										className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-									>
-										<div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-											<GalleryVerticalEnd className="size-4" />
-										</div>
-										<div className="flex flex-col gap-0.5 leading-none">
-											<span className="font-semibold">Beyondata</span>
-											<span className="">赋范空间</span>
-										</div>
-										<ChevronsUpDown className="ml-auto" />
-									</SidebarMenuButton>
-								</DropdownMenuTrigger>
-								<DropdownMenuContent className="w-[--radix-dropdown-menu-trigger-width]" align="start">
-									{data.versions.map(version => (
-										<DropdownMenuItem key={version} onSelect={() => setSelectedVersion(version)}>
-											v{version} {version === selectedVersion && <Check className="ml-auto" />}
-										</DropdownMenuItem>
-									))}
-								</DropdownMenuContent>
-							</DropdownMenu>
+							<SidebarMenuButton
+								size="lg"
+								className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+							>
+								<div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+									<GalleryVerticalEnd className="size-4" />
+								</div>
+								<div className="flex flex-col gap-0.5 leading-none">
+									<span className="font-semibold">Beyondata</span>
+									<span className="">赋范空间</span>
+								</div>
+							</SidebarMenuButton>
 						</SidebarMenuItem>
 					</SidebarMenu>
 					<form>
 						<SidebarGroup className="py-0">
 							<SidebarGroupContent className="relative">
-								<Label htmlFor="search" className="sr-only">
-									Search
-								</Label>
-								<SidebarInput id="search" placeholder="Search the docs..." className="pl-8" />
-								<Search className="pointer-events-none absolute left-2 top-1/2 size-4 -translate-y-1/2 select-none opacity-50" />
+								<Button className="w-full justify-start">
+									<Plus />
+									新建对话
+								</Button>
 							</SidebarGroupContent>
 						</SidebarGroup>
 					</form>
@@ -236,31 +225,18 @@ export default function ChatLayout() {
 					))}
 				</SidebarContent>
 				<SidebarRail />
+				<SidebarFooter>
+					<Button variant="ghost" className="flex items-center justify-between flex-row">
+						知识库会话
+						<CirclePlus />
+					</Button>
+					<Button variant="ghost" className="flex items-center justify-between flex-row">
+						数据库会话
+						<CirclePlus />
+					</Button>
+				</SidebarFooter>
 			</Sidebar>
 			<SidebarInset>
-				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-					<SidebarTrigger className="-ml-1" />
-					<Separator orientation="vertical" className="mr-2 h-4" />
-					<Breadcrumb>
-						<BreadcrumbList>
-							<BreadcrumbItem className="hidden md:block">
-								<BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
-							</BreadcrumbItem>
-							<BreadcrumbSeparator className="hidden md:block" />
-							<BreadcrumbItem>
-								<BreadcrumbPage>Data Fetching</BreadcrumbPage>
-							</BreadcrumbItem>
-						</BreadcrumbList>
-					</Breadcrumb>
-				</header>
-				{/* <div className="flex flex-1 flex-col gap-4 p-4">
-					<div className="grid auto-rows-min gap-4 md:grid-cols-3">
-						<div className="aspect-video rounded-xl bg-muted/50" />
-						<div className="aspect-video rounded-xl bg-muted/50" />
-						<div className="aspect-video rounded-xl bg-muted/50" />
-					</div>
-					<div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-				</div> */}
 				<Outlet />
 			</SidebarInset>
 		</SidebarProvider>
